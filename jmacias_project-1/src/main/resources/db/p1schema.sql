f3ab12388b6cfdbe7cd189251ec47aed3b9726fc
mzmzmz <44877972+JasonSMacias@users.noreply.github.com>
2019-07-16 13:06:12 -0400

    set up connection factory, refined SQL table-creation script, added sequences and triggers

--- jmacias_project-1/src/main/resources/db/p1schema.sql
+++ jmacias_project-1/src/main/resources/db/p1schema.sql
@@ -1,31 +1,77 @@
--- DROP TABLE Employee;
--- DROP TABLE Manager;
--- DROP TABLE ReimbReq;
+DROP TABLE img;
+DROP TABLE ReimbReq;
+DROP TABLE Employee;
 
 CREATE TABLE Employee (
-  usr_id NUMBER NOT NULL PRIMARY KEY,
-  password VARCHAR(60),
-  security_q VARCHAR(200),
-  isManager Boolean(),
+  usr_id INTEGER NOT NULL PRIMARY KEY,
+  pword VARCHAR2(60) NOT NULL,
+  security_q VARCHAR2(200) NOT NULL,
+  isManager CHAR(1) NOT NULL
 );
 
 CREATE TABLE ReimbReq (
-  req_id NUMBER PRIMARY KEY;
-  emp_id NUMBER FOREIGN KEY REFERENCES Employee(usr_id);
-  -- value
-  -- description
-  -- date
-  //
-  status VARCHAR(15),
+  req_id INTEGER NOT NULL PRIMARY KEY,
+  emp_id INTEGER NOT NULL,
+  value NUMBER NOT NULL,
+  description VARCHAR2(200),
+  date_requested DATE NOT NULL,
+  status VARCHAR(15) NOT NULL,
+  CONSTRAINT ReimbReq_fk FOREIGN KEY (emp_id) REFERENCES Employee(usr_id)
 );
 
 CREATE TABLE img (
-  img_id NUMBER,
-  req_id NUMBER FOREIGN KEY REFERENCES ReimbReq(req_id);
-  imgFilePath VARCHAR(100),
+  img_id INTEGER NOT NULL PRIMARY KEY,
+  req_id INTEGER NOT NULL,
+  imgFile BLOB NOT NULL,
+  CONSTRAINT img_fk FOREIGN KEY (img_id) REFERENCES ReimbReq(req_id)
 );
 /
 
--- sequence here
 
--- trigger here
+-- sequences here
+ DROP SEQUENCE emp_id_seq;
+CREATE SEQUENCE emp_id_seq
+  START WITH 1
+  INCREMENT BY 1
+;
+ DROP SEQUENCE req_id_seq;
+CREATE SEQUENCE req_id_seq
+  START WITH 1
+  INCREMENT BY 1
+;
+ DROP SEQUENCE img_id_seq;
+CREATE SEQUENCE img_id_seq
+    START WITH 1
+    INCREMENT BY 1
+;
+
+-- triggers here
+CREATE OR REPLACE TRIGGER emp_seq_trigger
+BEFORE INSERT ON Employee
+FOR EACH ROW
+BEGIN
+    IF :new.usr_id IS NULL
+    THEN
+        SELECT emp_id_seq.nextval INTO :new.usr_id FROM dual;
+    END IF;
+END;
+/
+CREATE OR REPLACE TRIGGER req_seq_trigger
+BEFORE INSERT ON ReimbReq
+FOR EACH ROW
+BEGIN
+    IF :new.req_id IS NULL
+    THEN
+        SELECT req_id_seq.nextval INTO :new.req_id FROM dual;
+    END IF;
+END;
+/
+CREATE OR REPLACE TRIGGER img_seq_trigger
+BEFORE INSERT ON img
+FOR EACH ROW
+BEGIN
+    IF :new.img_id IS NULL
+    THEN
+        SELECT img_id_seq.nextval INTO :new.img_id FROM dual;
+    END IF;
+END;
