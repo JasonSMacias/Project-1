7928bb508e062f314f1ba36375dfd80c148b4f5c
mzmzmz <44877972+JasonSMacias@users.noreply.github.com>
2019-07-24 00:40:03 -0400

    Index html and js for login/signup, got login working with servlet/db and returned response to console (signup only partially done)

--- jmacias_project-1/src/main/java/servlets/Project1Servlet.java
+++ jmacias_project-1/src/main/java/servlets/Project1Servlet.java
@@ -1,5 +1,6 @@
 package servlets;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -29,6 +30,7 @@ public class Project1Servlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		String route = request.getParameter("route");
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 		response.getWriter().append("\nEmployees: \n");
 		Employee testUser = uDao.getUser(3);
@@ -41,11 +43,39 @@ public class Project1Servlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String password = request.getParameter("password");
-		String userName = request.getParameter("name");
+		String password = "";
+		String userName = "";
+		String option = "";
+		
+		BufferedReader breader = request.getReader();
+		String req = breader.readLine();
+		String[] params = req.split("&");
+		String[] keys = new String[3];
+		String[] vals = new String[3];
+		for (int i = 0; i < params.length; i++) {
+			String[] temp = params[i].split("=");
+			keys[i] = temp[0];
+			vals[i] = temp[1];
+		}
+		for (int i = 0; i < keys.length; i++) {
+			if (keys[i].equals("name")) {
+				userName = vals[i];
+			}
+			else if (keys[i].equals("password")) {
+				password = vals[i];
+			}
+			else if (keys[i].equals("option")) {
+				option = vals[i];
+			}
+		}
 		Employee user = uDao.getUserByName(userName);
-		if (password.equals(user.getpWord())) {
+		if (option.equals("signup")) {
+			response.getWriter().append("Sorry, signUp has not been implemented yet");
+			System.out.println("signup");
+		}
+		else if (password.equals(user.getpWord())) {
 			response.getWriter().append("Sign in was successful!");
+			System.out.println("Success");
 		}
 		else {
 			response.getWriter().append("Sign in was not successful!");
--- jmacias_project-1/src/main/webapp/assets/logic.js
+++ jmacias_project-1/src/main/webapp/assets/logic.js
@@ -1,10 +1,14 @@
 const modalHTM = document.querySelector("#modal");
-const modalTitleHTM = document.querySelector("modal-title");
-const modalContentHTM = document.querySelector("modal-content");
+const modalTitleHTM = document.querySelector("#modal-title");
+const nameInput = document.querySelector("#name-input");
+const pwInput = document.querySelector("#pw-input");
 const closerHTM = document.querySelector(".closer");
 
 const loginButtonHTM = document.querySelector("#login-button");
 const signupButtonHTM = document.querySelector("#signup-button");
+const formSubmit = document.querySelector("#form-submit");
+
+const xhr = new XMLHttpRequest();
 
 function toggleModal() {
   if (toggleBoolean) {
@@ -15,7 +19,37 @@ function toggleModal() {
   }
   toggleBoolean = !toggleBoolean;
 }
+let option;
+function loginModal() {
+  modalTitleHTM.textContent = "Please Log In";
+  // nameInput.setAttribute("name", "user-name");
+  option = "login";
+  // nInputHTM.setAttribute("name", "user-name");
+  
+  toggleModal();
+}
+function signUpModal() {
+  modalTitleHTM.textContent = "Please Sign Up";
+  option = "signup";
+  toggleModal();
+}
+
+formSubmit.addEventListener("click", function(e){
+  e.preventDefault();
+  console.log("clicked");
+  xhr.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+    	console.log(xhr.responseText);
+    }
+  }
+  xhr.open("POST", "http://localhost:8080/jmacias_project-1/api/Project1Servlet");
+  let uName = document.querySelector("#name-input").value;
+  let pWord = document.querySelector("#pw-input").value;
+  console.log(`name=${uName}&password=${pWord}&option=${option}`);
+  xhr.send("name=" + uName+ "&password=" + pWord + "&option=" + option);
+});
+
 let toggleBoolean = false;
-loginButtonHTM.addEventListener("click", toggleModal);
-signupButtonHTM.addEventListener("click", toggleModal);
+loginButtonHTM.addEventListener("click", loginModal);
+signupButtonHTM.addEventListener("click", signUpModal);
 closerHTM.addEventListener("click", toggleModal);
